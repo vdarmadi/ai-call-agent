27a356b99b41684dacb0448831341559f1f9a0f0
vdarmadi <victor@sellgo.com>
2023-09-06 23:18:44 -0700

    Fix parser

--- clinic/phoneagent/parser.py
+++ clinic/phoneagent/parser.py
@@ -5,26 +5,21 @@ def parse_appointment_detail(conversation: str):
     lines = conversation.split('\n')
 
     # Regular expression for matching the doctor's name
-    doctor_name_pattern = r'I have scheduled.*Doctor\s+([A-Za-z]+)'
-    doctor_name_pattern_2 = r"Thank you for choosing Doctor\s+([A-Za-z]+)"
+    doctor_name_pattern = r'Doctor\s+([A-Za-z]+)'
 
     # Regular expression for matching the date and time
-    date_time_pattern = r'I have scheduled.*(\b(?:January|February|March|April|May|June|July|August|September|October' \
+    date_time_pattern = r'(\b(?:January|February|March|April|May|June|July|August|September|October' \
                         r'|November|December)\s+\d{1,2}(?:st|nd|rd|th)?\s+at\s+\d{1,2}(?::\d{2})?\s*(?:am|pm)?)'
 
     doctor_name = ""
     appointment_date_time = ""
-    for line in lines:
+    for line in reversed(lines):
         if "HUMAN" in line:
             continue
         # Find the doctor's name
         doctor_match = re.search(doctor_name_pattern, line)
         if doctor_match:
             doctor_name = doctor_match.group(1)
-        else:
-            doctor_match = re.search(doctor_name_pattern_2, line)
-            if doctor_match:
-                doctor_name = doctor_match.group(1)
 
         # Find the date and time
         date_time_match = re.search(date_time_pattern, line)
